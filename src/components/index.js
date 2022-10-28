4f7c31c9ac8e63d8669a7e61dc4aa6be6940ed21
Adeyemi Akinyemi <89089354+AdeDevs@users.noreply.github.com>
2022-10-28 21:50:56 +0100

    Updated

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAEpXuWDy8gdPUPXdm2VsPAh6B9QrtHAcU",
+  authDomain: "kryptykcalculator.firebaseapp.com",
+  projectId: "kryptykcalculator",
+  storageBucket: "kryptykcalculator.appspot.com",
+  messagingSenderId: "514109519433",
+  appId: "1:514109519433:web:604e7b2f02159d1e4c3198"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
